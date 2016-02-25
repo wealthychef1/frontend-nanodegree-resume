4fe77988ab8d4e6f5122f84f282b43df9eda72ed
Rich Cook <cook47@llnl.gov>
2016-02-24 16:23:18 -0800

    Resume sections added but not fully populated.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,75 @@
 /*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var formattedName = HTMLheaderName.replace("%data%", "Rich Cook");
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+$("#header").append(formattedName).append(formattedRole);
+*/
+var work = {
+  "jobs": [
+  {
+    "employer": "",
+    "title": "",
+    "location": "",
+    "startDate": 1900,
+    "endDate": 2000,
+    "description": ""
+  }
+  ]
+};
+var projects = {
+  "projects": [
+  {
+    "title": "",
+    "startDate": 1900,
+    "endDate": 2000,
+    "description": "",
+    "images": []
+  }
+  ]
+};
+var bio = {
+  "name": "Richard Cook",
+  "role": "Front End Developer",
+  "welcomeMessage": "Hello, I like to eat ice cream.",
+  "skills": [""],
+  "contacts": {
+    "mobile": "925-784-3077",
+    "email": "wealthychef@gmail.com",
+    "github": "wealthychef1",
+    "twitter": "wealthychef",
+    "location": "Livermore, CA, USA"
+  }
+};
+var education = {
+  "schools": [
+  {
+    "name": "Santa Rosa Junion College",
+    "city": "Santa Rosa, CA, US",
+    "degree": "AS",
+    "major": ["Chemistry"],
+    "graduation": 1982
+  },
+  {
+    "name": "UC Davis",
+    "city": "Davis, CA, US",
+    "degree": "BS",
+    "major": ["Chemical Engineering"],
+    "graduation": 1986
+  },
+  {
+    "name": "UC Davis",
+    "city": "Davis, CA, US",
+    "degree": "MS",
+    "major": ["Computer Science and Engineering"],
+    "graduation": 1990
+  }
+  ],
+  "onlineCourses": [
+  {
+    "title": "",
+    "school": "",
+    "startDate": 1900,
+    "endDate": 2000,
+    "url": ""
+  }
+  ]
+};
